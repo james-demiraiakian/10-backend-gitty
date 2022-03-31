e31ebb53c1b960dff966a708e8efd93266e589fe
James Demiraiakian <james.armen@gmail.com>
2022-03-31 10:43:33 -0700

    added test for redirect after OAuth

--- __tests__/github.test.js
+++ __tests__/github.test.js
@@ -21,4 +21,20 @@ describe('10-gackend-gitty github user routes', () => {
       /https:\/\/github.com\/login\/oauth\/authorize\?client_id=[\w\d]+&scope=user&redirect_uri=http:\/\/localhost:7890\/api\/v1\/github\/login\/callback/i
     );
   });
+
+  it('should redirect to /api/v1/github/dashboard after github OAuth', async () => {
+    const req = await request
+      .agent(app)
+      .get('/api/v1/github/login/callback?code=42')
+      .redirects(1);
+
+    expect(req.body).toEqual({
+      id: expect.any(String),
+      username: 'fake_github_user',
+      email: 'not-real@example.com',
+      avatar: expect.any(String),
+      iat: expect.any(Number),
+      exp: expect.any(Number),
+    });
+  });
 });
