fd3f72d56e0c56e2d500cf30e6aa42fc566a4382
James Demiraiakian <james.armen@gmail.com>
2022-04-07 11:23:54 -0700

    the refactor was so frustrating that I forgot to ACP inbetween fixing things. Got all code refactored. Working on writing new quotes route

---
+++ __tests__/quotes.test.js
@@ -0,0 +1,32 @@
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+const pool = require('../lib/utils/pool');
+
+const agent = request.agent(app);
+
+jest.mock('../lib/utils/github.js');
+
+describe('github quotes route', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  afterAll(() => {
+    pool.end();
+  });
+
+  it('gets quotes', async () => {
+    await agent.get('/api/v1/github/login/callback?code=42').redirects(1);
+
+    const expected = [
+      { author: expect.any(String), content: expect.any(String) },
+      { author: expect.any(String), content: expect.any(String) },
+      { author: expect.any(String), content: expect.any(String) },
+    ];
+
+    const res = await agent.get();
+
+    expect(res.body).toEqual(expected);
+  });
+});
